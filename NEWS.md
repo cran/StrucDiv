74b32d6d08de2adebeaed28886ddc120027a0b3f
Leila Schuh <l.schuh@posteo.net>
2022-08-11 09:10:14 +0000

    version 0.1.1

---
+++ NEWS.md
@@ -0,0 +1,11 @@
+---
+output:
+  html_document: default
+  pdf_document: default
+---
+# StrucDiv 0.1.1
+
+ BUG FIX
+
+*  Adressing warning from r-devel-linux-x86_64-debian-clang and r-devel-linux-x86_64-fedora-clang: 'use of bitwise ```&``` with boolean operands'. We replaced ```&``` with ```&&```.
+
--- R/diversity_switch_function.R
+++ R/diversity_switch_function.R
@@ -8,28 +8,27 @@
 #' Shannon entropy has a window-dependent maximum.
 #' Additionally, the value gradient is considered when \code{delta} = "1" or \code{delta} = "2". 
 #' The values of structural diversity entropy with \code{delta} = "1" or \code{delta} = "2" are not restricted and depend on the values of the input raster.
-#' entropyNorm is Shannon entropy normalized over maximum entropy, which depends on the size of the moving window. The metric entropyNorm ranges between 0 and 1.
-#' Contrast, dissimilarity and homogeneity are established second-order texture metrics. 
-#' Contrast and dissimilarity consider the value gradient, their values are not restricted and depend on the values of the input raster.
-#' Homogeneity quantifies the closeness of empirical probabilities to the diagonal and ranges between 0 and 1. 
-#' Homogeneity is 1 when all pixel pairs are the same and approaches  0 as differences increase.
+#' The metric \code{entropyNorm} is Shannon entropy normalized over maximum entropy, which depends on the size of the moving window. The metric \code{entropyNorm} ranges between 0 and 1.
+#' The metrics \code{contrast}, \code{dissimilarity} consider the value gradient, their values are not restricted and depend on the values of the input raster.
+#' The metric \code{homogeneity} quantifies the closeness of empirical probabilities to the diagonal and ranges between 0 and 1. 
+#' the metric \code{homogeneity} is 1 when all pixel pairs are the same and approaches 0 as differences increase.
 #' @param rank logical. Should values be replaced with ranks in each gray level co-occurrence 
-#' matrix (GLCM)? Defaults to FALSE.
-#' @param delta character, takes 3 options: "0", "1", or "2". 
-#' Delta is the difference weight parameter, 
+#' matrix (GLCM)? Defaults to \code{FALSE}.
+#' @param delta character, takes 3 options: \code{"0"},\code{ "1"}, or \code{"2"}. 
+#' The parameter \code{delta} is the difference weight parameter, 
 #' it defines how the differences between pixel values within a pixel pair should be weighted.  
-#' If rank = TRUE, delta defines how the differences between ranks should be weighted.  
-#' The default value is "0" (no weight). Set delta = "1" for absolute difference weight, 
-#' or delta = "2" for squared difference weight. 
-#' The delta parameter can only be set when the metric entropy is used. 
-#' Dissimilarity automatically employs delta = "1", and contrast employs delta = "2".
+#' If \code{rank = TRUE}, delta defines how the differences between ranks should be weighted.  
+#' The default value is \code{"0"} (no weight). Set \code{delta = "1"} for absolute difference weight, 
+#' or \code{delta = "2"} for squared difference weight. 
+#' The \code{delta} parameter can only be set when the metric \code{entropy} is used. 
+#' The metric \code{dissimilarity} automatically employs \code{delta = "1"}, and \code{contrast} employs \code{delta = "2"}.
 #' @param SpatMat the GLCM that is returned by an internal function
-#' to the \code{StrucDiv} function. 
+#' to the \code{\link{StrucDiv}} function. 
 #' @param Hetx the spatial structural diversity matrix that is returned by an internal function
-#' to the \code{StrucDiv} function. the spatial structural diversity metric is calculated on every element
+#' to the \code{\link{StrucDiv}} function. the spatial structural diversity metric is calculated on every element
 #' of the GLCM, which generates the spatial structural diversity matrix \code{Hetx}. The sum of this
 #' matrix represents the spatial structural diversity estimate of the moving window, 
-#' the size of which is defined by \code{wsl} in the \code{StrucDiv} function.
+#' the size of which is defined by \code{wsl} in the \code{\link{StrucDiv}} function.
 #' @param nrp integer. The total number of pixel pairs, which is calculated 
 #' internally and passed to the spatial structural diversity metric functions.
 #' The total number of pixel pairs is defined by the size of the moving window (\code{wsl x wsl}), 
@@ -37,7 +36,7 @@
 #' @param narm logical. Defines how missing values are treated, and is automatically set to 0 if na.handling = na.pass, and to 1 if na.handling = na.omit.
 #' @param display_progress logical. If TRUE the progress bar is displayed.
 #' @param ... possible further arguments.
-#' @details This function is used internally and is called as an argument to \code{StrucDiv}.
+#' @details This function is used internally and is called as an argument to \code{\link{StrucDiv}}.
 #' @return The output is a raster layer with the same dimensions as the input raster and is called a (spatial) structural diversity map. 
 #' It represents spatial structural diversity quantified on the spatial scale that is defined by the size of the moving window. 
 #' When \code{na.handling = na.pass}, then the output map will have an NA-edge of 0.5*(\code{wsl}-1), 
